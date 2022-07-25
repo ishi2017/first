537beaa5249f92461d879b438bb04e08a7023ee0
ishi2017 <amritpalmittal@gmail.com>
2022-07-25 18:09:51 +0530

    chitchat updated

--- chitchat/lib/screen/chat.dart
+++ chitchat/lib/screen/chat.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,32 +8,40 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: Firebase.initializeApp(),
-      builder: (cntx, snap) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Chitchat'),
-        ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            padding: const EdgeInsets.all(8),
-            child: const Text('This works'),
-          ),
-        ),
-        floatingActionButton: IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () {
-            FirebaseFirestore.instance
-                .collection('chats/kumPdJau69q1WorEv56y/message')
-                .snapshots()
-                .listen((data) {
-              data.docs.forEach((msg) {
-                print(msg['text']);
-              });
-            });
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chitchat'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/kumPdJau69q1WorEv56y/message')
+            .snapshots(),
+        builder: (cntx, AsyncSnapshot<QuerySnapshot> streamSnapShot) {
+          final data = streamSnapShot.data?.docs;
+          print(streamSnapShot.data);
+          if (streamSnapShot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else {
+            return ListView.builder(
+              itemCount: data != null ? streamSnapShot.data?.docs.length : 0,
+              itemBuilder: (context, index) => Container(
+                padding: const EdgeInsets.all(8),
+                child: Text(streamSnapShot.data?.docs[index]['text']),
+              ),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.amber,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/kumPdJau69q1WorEv56y/message')
+              .add({
+            'text': 'This is added dynamically',
+          });
+        },
       ),
     );
   }
